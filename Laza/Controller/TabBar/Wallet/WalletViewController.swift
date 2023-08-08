31eaabd9f3b853ce50182f2e36e4d476f16ad8af
learning <perdiyansyah111@gmail.com>
2023-08-08 08:42:15 +0700

    push selasa 8 agustus 2023

---
+++ Laza/Controller/TabBar/Wallet/WalletViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WalletViewController.swift
+//  Laza
+//
+//  Created by Perdi Yansyah on 08/08/23.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
