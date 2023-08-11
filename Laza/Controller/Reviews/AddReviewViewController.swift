4f297dc8e8f4da7fa6eaa94c70ebc8c637b1bc6a
learning <perdiyansyah111@gmail.com>
2023-08-11 15:23:48 +0700

    terbaru

--- Laza/Controller/Reviews/AddReviewViewController.swift
+++ Laza/Controller/Reviews/AddReviewViewController.swift
@@ -8,24 +8,33 @@
 import UIKit
 
 class AddReviewViewController: UIViewController {
-    
+
+    @IBOutlet weak var rating: UILabel!
+    private let ratingLb: Float = 0.0
     @IBOutlet weak var slider: CustomSlider! {
         didSet {
             slider.sliderHeight = CGFloat(18)
+            slider.minimumValue = 0.0
+            slider.maximumValue = 5.0
+            slider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
         }
     }
     @IBOutlet weak var viewBack: UIView!
-    
+
     @IBAction func backBtn(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        slider.value = ratingLb
         viewBack.layer.cornerRadius = viewBack.bounds.height / 2.0
         viewBack.clipsToBounds = true
     }
-    
-    
-}
 
+    @objc func sliderValueChanged(_ sender: CustomSlider) {
+        let value = sender.value
+        rating.text = String(format: "%.1f", value) // Mengubah teks label rating dengan nilai slider
+    }
+}
