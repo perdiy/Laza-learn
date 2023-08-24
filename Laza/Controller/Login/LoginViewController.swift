c927585f19fd5f4f09f573cb821534b6a5771532
learning <perdiyansyah111@gmail.com>
2023-08-24 15:22:27 +0700

    sign in google

--- Laza/Controller/Login/LoginViewController.swift
+++ Laza/Controller/Login/LoginViewController.swift
@@ -5,46 +5,52 @@
 //  Created by Perdi Yansyah on 26/07/23.
 //
 
-import UIKit
-
-class LoginViewController: UIViewController {
-
-    // Button IB
-    @IBAction func facebookBtn(_ sender: Any) {
-    }
-    
-    @IBAction func twitterBtn(_ sender: Any) {
-    }
-    
-    @IBAction func googleBtn(_ sender: Any) {
-    }
-    
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-
-    }
-    @IBAction func createBtn(_ sender: Any) {
-
-        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
-        if let signupVC = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController {
-            navigationController?.pushViewController(signupVC, animated: true)
-        }
-    }
-    
-    @IBAction func signInBtn(_ sender: Any) {
-     
-        let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
-        if let welcomeVC = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
-            navigationController?.pushViewController(welcomeVC, animated: true)
-        }
-    }
-        
-    
-}
- 
+//import UIKit
+//import GoogleSignIn
+//
+//class LoginViewController: UIViewController {
+//
+//    // Button IB
+//    @IBAction func facebookBtn(_ sender: Any) {
+//    }
+//
+//    @IBAction func twitterBtn(_ sender: Any) {
+//    }
+//
+//    @IBAction func googleBtn(_ sender: Any) {
+//        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+//            guard error == nil else { return }
+//            guard let signInResult = signInResult else { return }
+//            print("Sign in success: \(String(describing: signInResult.user.profile?.email))")
+//        }
+//    }
+//
+//
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//
+//
+//    }
+//    @IBAction func createBtn(_ sender: Any) {
+//
+//        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+//        if let signupVC = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController {
+//            navigationController?.pushViewController(signupVC, animated: true)
+//        }
+//    }
+//
+//    @IBAction func signInBtn(_ sender: Any) {
+//
+//        let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
+//        if let welcomeVC = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
+//            navigationController?.pushViewController(welcomeVC, animated: true)
+//        }
+//    }
+//
+//
+//}
+//
--- Laza/Controller/Login/ViewController.swift
+++ Laza/Controller/Login/ViewController.swift
@@ -6,44 +6,46 @@
 //
 
 import UIKit
+import GoogleSignIn
+
 
 class ViewController: UIViewController {
     
-    // Button IB
-    @IBAction func facebookBtn(_ sender: Any) {
-    }
-    
-    @IBAction func twitterBtn(_ sender: Any) {
-    }
-    
+    // Google Button sign in with Google
     @IBAction func googleBtn(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            guard let signInResult = signInResult else { return }
+            print("Sign in success: \(String(describing: signInResult.user.profile?.email))")
+        }
     }
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLoggedIn()
     }
     
+    // cek sudah login atau belum dengan user token
     private func checkLoggedIn() {
-            if let userToken = UserDefaults.standard.string(forKey: "userToken") {
-                let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
-                if let myProfileVC = storyboard.instantiateViewController(withIdentifier: "TabBarViewController") as? TabBarViewController {
-                    myProfileVC.userToken = userToken
-                    
-                    navigationController?.pushViewController(myProfileVC, animated: false)
-                }
+        if let userToken = UserDefaults.standard.string(forKey: "userToken") {
+            let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
+            if let myProfileVC = storyboard.instantiateViewController(withIdentifier: "TabBarViewController") as? TabBarViewController {
+                myProfileVC.userToken = userToken
+                
+                navigationController?.pushViewController(myProfileVC, animated: false)
             }
         }
+    }
+    
+    // Sign Up Button
     @IBAction func SignUpButton(_ sender: Any) {
         let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
         if let signupVC = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController {
             navigationController?.pushViewController(signupVC, animated: true)
         }
     }
+    
+    // Sign In Button
     @IBAction func SignInButton(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
         if let welcomeVC = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController {
